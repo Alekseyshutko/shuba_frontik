f25770117145614658d6fcf86c8d2369bc1eb7dd
Alekseyshutko <Aleksey_shutko@rambler.ru>
2022-06-26 20:45:04 +0300

    look

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask import render_template, redirect, url_for, make_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
